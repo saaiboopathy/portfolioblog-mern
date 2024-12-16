ca9dec2e4b4da6bf64778ba1e9c40c46d7114d60
saaiboopathy <saaisajith11@gmail.com>
2024-12-16 12:59:02 +0530

    modified port

--- backend/index.js
+++ backend/index.js
@@ -92,8 +92,8 @@ app.post('/api/blogs', async (req, res) => {
 
 
 
+const port = process.env.PORT || 5000;
 
-
-app.listen(5000, () => {
+app.listen(port, () => {
     console.log("The server is running in port 5000")
 })
