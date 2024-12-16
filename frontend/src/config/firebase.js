ec0dc3654df9fb1acd068557433d472e59d9de73
saaiboopathy <saaisajith11@gmail.com>
2024-12-16 12:46:32 +0530

    update env frontend

--- frontend/src/config/firebase.js
+++ frontend/src/config/firebase.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBQXlKpCGMV2nuKU0f6M1QM-8FLQSR5JUA",
-  authDomain: "blogapp-f8ee8.firebaseapp.com",
-  projectId: "blogapp-f8ee8",
-  storageBucket: "blogapp-f8ee8.firebasestorage.app",
-  messagingSenderId: "408592815822",
-  appId: "1:408592815822:web:485f065d01732092d8a29f"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
