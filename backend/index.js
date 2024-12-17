6e3da610551e88b492ff7c0986e1180661bb584c
saaiboopathy <saaisajith11@gmail.com>
2024-12-17 12:12:02 +0530

    minor update

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,8 @@ app.use(cors())
 
 //Connecting MongoDB
 mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
     serverSelectionTimeoutMS: 30000,
 }).then(() => {
     console.log("MongoDB Connected");
